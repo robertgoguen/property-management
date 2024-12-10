5cdea1c955bad023f9aef3047d5eb0905954794f
Robert Goguen <rgoguen@cisco.com>
2024-12-10 12:02:35 -0500

    testing

--- src/main/java/com/mycompany/property_management/controller/PropertyController.java
+++ src/main/java/com/mycompany/property_management/controller/PropertyController.java
@@ -30,7 +30,6 @@ public class PropertyController {
    @PostMapping("/properties")
    public ResponseEntity<PropertyDTO>  saveProperty(@RequestBody PropertyDTO propertyDTO){
       propertyDTO = propertyService.saveProperty(propertyDTO);
-
       ResponseEntity<PropertyDTO>  responseEntity = new ResponseEntity<>(propertyDTO, HttpStatus.CREATED);
       return responseEntity;
    }
