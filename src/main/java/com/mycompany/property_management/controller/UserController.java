b8bee0cb1f840f9f3ef0dffba1a19b6a8e05f4d6
Robert Goguen <rgoguen@cisco.com>
2024-12-13 09:12:20 -0500

    implemented error handling using AOP

--- src/main/java/com/mycompany/property_management/controller/UserController.java
+++ src/main/java/com/mycompany/property_management/controller/UserController.java
@@ -3,6 +3,7 @@ package com.mycompany.property_management.controller;
 import com.mycompany.property_management.dto.PropertyDTO;
 import com.mycompany.property_management.dto.UserDTO;
 import com.mycompany.property_management.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class UserController {
    private UserService userService;
 
    @PostMapping("/register")
-   public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO) {
+   public ResponseEntity<UserDTO> register(@Valid @RequestBody UserDTO userDTO) {
       userDTO = userService.register(userDTO);
       return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
    }
--- src/main/java/com/mycompany/property_management/exception/CustomExceptionHandler.java
+++ src/main/java/com/mycompany/property_management/exception/CustomExceptionHandler.java
@@ -1,15 +1,37 @@
 package com.mycompany.property_management.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+   private final Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
+
+   @ExceptionHandler(MethodArgumentNotValidException.class)
+   public ResponseEntity<List<ErrorModel>> handleFieldValidation(MethodArgumentNotValidException manv) {
+      List<ErrorModel> errorModelList = new ArrayList<>();
+      List<FieldError>  fieldErrorList = manv.getBindingResult().getFieldErrors();
+      ErrorModel errorModel;
+
+      for(FieldError fe: fieldErrorList) {
+         logger.error("Inside field validation: {} - {}", fe.getField(), fe.getDefaultMessage());
+         errorModel = new ErrorModel();
+         errorModel.setCode(fe.getField());
+         errorModel.setMessage(fe.getDefaultMessage());
+         errorModelList.add(errorModel);
+      }
+      return new ResponseEntity<List<ErrorModel>>(errorModelList, HttpStatus.BAD_REQUEST);
+   }
    @ExceptionHandler(BusinessException.class)
    public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException businessException) {
       System.out.println("BusinessException is thrown");
